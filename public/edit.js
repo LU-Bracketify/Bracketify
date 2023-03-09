dd55ed9379d7b3b3aac75cd088bec8c2338c66a3
Andrew Moser <amoser@liberty.edu>
2023-03-08 23:15:13 -0500

    beginning js file

---
+++ public/edit.js
@@ -0,0 +1,8 @@
+var type = ['single elim','double elim','rr','group']
+var name = 'Sample Name'
+var numTeams = [4,10,25,50]
+var isSeeded = [true, false]
+
+for (var i=0; i < type.length; i++) {
+    
+}
