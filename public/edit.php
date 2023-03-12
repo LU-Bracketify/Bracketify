98f9e0a38fb798bec02e975873100f797045ec47
Jonathan Sulgrove <jsulgrove216@gmail.com>
2023-03-12 18:18:55 -0500

    Readded manifest

--- public/edit.php
+++ public/edit.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +10,7 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="apple-touch-icon" type="image/png" href="images/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="images/apple-touch-icon.png">
+    <link rel="manifest" href="manifest.webmanifest">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
